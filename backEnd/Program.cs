d43d1b92780b75f5630807dd9ecb1a6de7f21b28
Vinicius Fortino <vinicius.fortino@gmail.com>
2023-05-07 00:02:42 -0300

    Update Program.cs

--- backEnd/Program.cs
+++ backEnd/Program.cs
@@ -1,5 +1,8 @@
+using backEnd.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,17 @@ namespace backEnd
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<EstocaaiContextDb>();
+                db.Database.Migrate(); // apply the migrations
+            }
+
+            host.Run(); // start handling requests
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
